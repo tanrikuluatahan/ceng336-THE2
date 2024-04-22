ecfe565c159700aea932cbdd654e2fb689229d72
atahan tanrikulu <atahantanrikulu@192.168.1.90>
2024-04-22 12:05:58 +0300

    lts update

--- main.c
+++ main.c
@@ -60,8 +60,8 @@
 // POLL THE PORTA<0-3>
 
 typedef enum LedOnOff{
-    LED_ON,
     LED_OFF,
+    LED_ON,
 }LedOnOff;
 
 typedef struct GridPosition {
@@ -97,7 +97,18 @@ typedef enum ControlKeys{
 
 ControlKeys controlKeys;
 
+typedef enum L_Rotate{
+    A,
+    B,
+    C,
+    D,
+} L_Rotate;
 
+L_Rotate l_rotate = 0;
+
+bool debounce_prev_submit = false;
+bool submit_flag = false;
+bool debounce_prev_rotate = false;
 
 // TETRIS BLOCKS WILL BE ROTATED BY THE PORTB-5
 // TETRIS BLOCKS WILL BE SUBMITTED BY THE PORTB-6
@@ -131,16 +142,21 @@ ControlKeys controlKeys;
 //          FUNCTIONS           //
 // ============================ //
 
-void move_left(){
+
+// GAME LOGIC
+
+void move_left(GameObjects object){
     GridPosition alivePosition;
     // alivePosition.x = gridPosition.x - 1;
-    // alivePosition.y = gridPosition.y;
+
     // REFERANCE => UPPER LEFT CORNER OF THE OBJECTS
     // only point object has the area size of 1x1.
     // others should be considered as 2x2.
-    alivePosition.x = gridPosition.x - 1;
-    if(gridPosition.x >=1)
+    
+    if(gridPosition.x >=1){
+        alivePosition.x = gridPosition.x - 1;
         gridPosition.x = alivePosition.x;
+    }
 }
 void move_right(GameObjects object){
     // REFERANCE => UPPER LEFT CORNER OF THE OBJECTS
@@ -150,7 +166,7 @@ void move_right(GameObjects object){
     switch (object)
     {
         case POINT_OBJECT:
-            if(gridPosition.x <=6)
+            if(gridPosition.x <=2)
             {
                 alivePosition.x = gridPosition.x + 1;
                 gridPosition.x = alivePosition.x;
@@ -161,17 +177,148 @@ void move_right(GameObjects object){
             // to check right bounds we need to check the
             // right bounds. L_OBJECT and SQUARE_OBJECTS
             // should checked for the same boundaries.
-            
+
+            // edge bound index: left=index<1> , right=index<2>
+            //              for current position
+            // after the shift, it would become:
+            //                   left=index<2>, right=index<3>
         case SQUARE_OBJECT:
-            alivePosition.x = gridPosition.x + 1;
-            alivePosition.x = gridPosition.x;
-            if(alivePosition.x <=6){
-                alivePosition.x = gridPosition.x + 2;
-                gridPosition.x = alivePosition.x;
+            // referance was the left tile, by adding one it 
+            // become right tile
+            // check the gridPosition.x(right tile) if it is in 
+            // the edge bounds.
+            if(gridPosition.x <=1){
+                // if so, 
+                alivePosition.x = gridPosition.x;
+                gridPosition.x = alivePosition.x + 1;
             }
             break;
     }
 }
+void move_up(GameObjects object){
+    GridPosition alivePosition;
+    // alivePosition.y = gridPosition.y - 1;
+
+    // REFERANCE => UPPER LEFT CORNER OF THE OBJECTS
+    // only point object has the area size of 1x1.
+    // others should be considered as 2x2.
+    if(gridPosition.y >=1){
+        alivePosition.y = gridPosition.y - 1;
+        gridPosition.y = alivePosition.y;
+    }
+}
+void move_down(GameObjects object){
+    GridPosition alivePosition;
+
+    switch (object)
+    {
+        case POINT_OBJECT:
+            if(gridPosition.y<=6){
+                alivePosition.y = gridPosition.y + 1;
+                gridPosition.y = alivePosition.y;
+            }
+            break;
+        case L_OBJECT:
+                // our referance was the upper left, in order 
+                // to check lower bounds we need to check the
+                // low bounds. L_OBJECT and SQUARE_OBJECTS
+                // should checked for the same boundaries.
+
+                // edge bound index: lower=index<6> , upper=index<5>
+                //              for current position
+                // after the shift, it would become:
+                //                   lower=index<7>, upper=index<6>
+        case SQUARE_OBJECT:
+            if(gridPosition.y<=5){
+                alivePosition.y = gridPosition.y;
+                gridPosition.y = alivePosition.y + 1;
+            }
+            break;
+    }
+}
+
+bool submit(){
+    submit_flag = false;
+    switch (gameObjects)
+    {
+    case POINT_OBJECT:
+        if(!gameGrid[gridPosition.x][gridPosition.y]){
+            gameGrid[gridPosition.x][gridPosition.y] = 1;
+            submit_flag = true;
+        }
+        break;
+    case SQUARE_OBJECT:
+        if(!gameGrid[gridPosition.x][gridPosition.y]&&
+            !gameGrid[gridPosition.x][gridPosition.y+1]&&
+            !gameGrid[gridPosition.x+1][gridPosition.y]&&
+            !gameGrid[gridPosition.x+1][gridPosition.y+1]){
+                gameGrid[gridPosition.x][gridPosition.y] = 1;
+                gameGrid[gridPosition.x][gridPosition.y+1] = 1;
+                gameGrid[gridPosition.x+1][gridPosition.y] = 1;
+                gameGrid[gridPosition.x+1][gridPosition.y+1] = 1;
+                submit_flag = true;
+            }
+        break;
+    case L_OBJECT:
+        // enum    **    **      *      *
+        //         *      *     **      **
+        //         (a)   (b)    (c)    (d)
+        switch(l_rotate){
+            case A:
+            {
+                if(!gameGrid[gridPosition.x][gridPosition.y]&&
+                    !gameGrid[gridPosition.x+1][gridPosition.y]&&
+                    !gameGrid[gridPosition.x][gridPosition.y+1]){
+
+                    gameGrid[gridPosition.x][gridPosition.y] = 1;
+                    gameGrid[gridPosition.x+1][gridPosition.y] = 1;
+                    gameGrid[gridPosition.x][gridPosition.y+1] = 1;
+                    submit_flag = true;
+                    }
+            }
+            case B:
+            {
+                if(!gameGrid[gridPosition.x][gridPosition.y]&&
+                    !gameGrid[gridPosition.x+1][gridPosition.y]&&
+                    !gameGrid[gridPosition.x+1][gridPosition.y+1]){
+
+                    gameGrid[gridPosition.x][gridPosition.y] = 1;
+                    gameGrid[gridPosition.x+1][gridPosition.y] = 1;
+                    gameGrid[gridPosition.x+1][gridPosition.y+1] = 1;
+                    submit_flag = true;
+                    }
+            }    
+            case C:
+            {
+                if(!gameGrid[gridPosition.x+1][gridPosition.y]&&
+                    !gameGrid[gridPosition.x][gridPosition.y+1]&&
+                    !gameGrid[gridPosition.x+1][gridPosition.y+1]){
+
+                    gameGrid[gridPosition.x+1][gridPosition.y] = 1;
+                    gameGrid[gridPosition.x][gridPosition.y+1] = 1;
+                    gameGrid[gridPosition.x+1][gridPosition.y+1] = 1;
+                    submit_flag = true;
+                    }
+                
+            }
+            case D:
+            {
+                if(!gameGrid[gridPosition.x][gridPosition.y]&&
+                    !gameGrid[gridPosition.x][gridPosition.y+1]&&
+                    !gameGrid[gridPosition.x+1][gridPosition.y+1]){
+
+                    gameGrid[gridPosition.x][gridPosition.y] = 1;
+                    gameGrid[gridPosition.x][gridPosition.y+1] = 1;
+                    gameGrid[gridPosition.x+1][gridPosition.y+1] = 1;
+                    submit_flag = true;
+                    }
+            }
+        }
+        break;
+    }
+    return submit_flag;
+}
+
 
 
 // You can write function definitions here...
@@ -183,6 +330,31 @@ __interrupt(high_priority)
 void HandleInterrupt()
 {
     // ISR ...
+    
+    if (INTCONbits.RBIF){
+        INTCONbits.RBIF = 0;
+
+        // sanity check for debouncing
+        if(PORTBbits.RB5){
+            if(!debounce_prev_rotate){
+                l_rotate = (l_rotate+1) % 4;
+            }
+        }
+        debounce_prev_rotate = PORTBbits.RB5;
+        if(PORTBbits.RB6){
+            if(!debounce_prev_submit){
+                if(submit()){
+                    gameObjects = (gameObjects + 1) % 3;
+                    gridPosition.x = 0;
+                    gridPosition.y = 0;
+                    l_rotate = 0;
+                }
+            }
+        }
+
+
+        
+    }
 }
 
 // ============================ //
